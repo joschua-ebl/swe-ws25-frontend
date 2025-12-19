25ddb284908ad9ce4d9b296b77371f12e559b646
joschua <joschua.ebel1337@gmail.com>
2025-12-19 08:32:55 +0100

    fix: config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'suche',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./features/login/login.component').then(m => m.LoginComponent),
+    },
+    {
+        path: 'suche',
+        loadComponent: () =>
+            import('./features/suche/suche.component').then(m => m.SucheComponent),
+    },
+    {
+        // WICHTIG: 'neu' muss VOR ':id' stehen, da ':id' sonst 'neu' matcht
+        path: 'buch/neu',
+        loadComponent: () =>
+            import('./features/anlegen/anlegen.component').then(m => m.AnlegenComponent),
+        canActivate: [authGuard],
+    },
+    {
+        path: 'buch/:id',
+        loadComponent: () =>
+            import('./features/detail/detail.component').then(m => m.DetailComponent),
+    },
+    {
+        path: '**',
+        redirectTo: 'suche',
+    },
+];
