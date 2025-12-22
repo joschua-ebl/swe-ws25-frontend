5bbe5320d3c4e4b8a6d9cb20c7e9321280232955
joschua <joschua.ebel1337@gmail.com>
2025-12-22 10:43:10 +0100

    feat: Zugriff Buchdaten

---
+++ src/app/core/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:3000',
+    keycloak: {
+        url: 'https://localhost:8880',
+        realm: 'nest',
+        clientId: 'nest-client',
+    },
+};
